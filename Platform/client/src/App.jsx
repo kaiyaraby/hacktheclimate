b3eaf8ba50ab8430e7e9a2737e5a5720dbd48d88
Ed Hamilton <ed@jehamilton.co.uk>
2024-03-18 11:14:28 +0000

    Added basic globe component.

--- Platform/client/src/App.jsx
+++ Platform/client/src/App.jsx
@@ -5,6 +5,57 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import { getExampleData } from './Service/Example';
+import Globe from "react-globe.gl";
+import EarthTexture from "./earthtexture.jpg";
+
+const WorkspaceComponent = (props) => {
+
+    const [points, setPoints] = useState([]);
+    const [globeSelectedArea, setGlobeSelectedArea] = useState([]);
+
+    const addPoint = (lat, lng) => {
+        let newPoints = points;
+        points.push([lng, lat]);
+        setPoints(newPoints);
+        setGlobeSelectedArea([{
+            type: "feature",
+            geometry: {
+                "type": "Polygon",
+                coordinates: [points]
+            }
+        }]);
+        console.log(globeSelectedArea);
+    };
+
+    return <RootInterfaceComponent globeSelectedArea={globeSelectedArea} addPoint={addPoint} />;
+};
+
+const RootInterfaceComponent = (props) => {
+
+    const onGlobeClick = ({lat, lng}, event) => {
+        console.log("Left Click at " + lat + ", " + lng);
+        props.addPoint(lat, lng);
+    };
+
+    const onGlobeRightClick = ({lat, lng}, event) => {
+        console.log("Right Click at " + lat + ", " + lng);
+    }
+    return <GlobeComponent onGlobeClick={onGlobeClick} onGlobeRightClick={onGlobeRightClick} globeSelectedArea={props.globeSelectedArea} />;
+};
+
+const GlobeComponent = (props) => {
+
+    return <Globe
+        globeImageUrl={EarthTexture}
+        showGraticules={true}
+        showAtmosphere={true}
+        onGlobeClick={props.onGlobeClick}
+        onGlobeRightClick={props.onGlobeRightClick}
+        polygonsData={props.globeSelectedArea}
+        polygonCapColor={() => 'rgba(200, 0, 0, 0.6)'}
+        polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+    />;
+}
 
 const TestComponent = () => {
 
@@ -54,6 +105,10 @@ const router = createBrowserRouter([
     {
         path: "/test",
         element: <TestComponent />,
+    },
+    {
+        path: "/workspace",
+        element: <WorkspaceComponent />,
     },
 ]);
 
