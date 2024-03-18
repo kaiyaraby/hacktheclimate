7ecc04dd157dc0d788950157b187fb1ea1b00cc3
PBrusset <47607449+PBrusset@users.noreply.github.com>
2024-03-18 11:18:57 +0000

    trying to understand js

--- Platform/client/src/index.jsx
+++ Platform/client/src/index.jsx
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const myElement = <h1>I Love JSX!</h1>;
+const Team =(<div>
+            <h2>Meet the Team</h2>
+            <p> whatever</p>
+            </div>)
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(Team);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
