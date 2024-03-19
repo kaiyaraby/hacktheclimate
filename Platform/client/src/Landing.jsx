546043f197a8d4122713c09e80e31adf90f89748
PBrusset <47607449+PBrusset@users.noreply.github.com>
2024-03-19 10:01:04 +0000

    cool react stuff looking good

--- Platform/client/src/Landing.jsx
+++ Platform/client/src/Landing.jsx
@@ -1,7 +1,30 @@
+import './App.scss';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import turbines from './Images/turbines.jpg';
+
 const LandingComponent = () => {
-    return <div>
-        <p>This is the landing page for our amazing app!</p>
+    return (
+    <div>
+
+    <div className="bg" />
+
+    <div class='text-on-image'>
+        <heady class='heady'> Green energy is bae </heady>
+        <p class='ohmy'>
+            Oh my god we're being such good humans and all, here's a few facts about something something?
+            ieiurg pegn eiug peiuhg pweurgejn geur e rfpieurf piufj ietgh hg eig rhpeiug rhpeiug rhpieuh geur
+            ieug hpe rugpeu gfhpeiu rgh!
+            <br />
+            <z> <a href="https://www.google.com">OMG A LINK!!</a> </z>
+        </p>
+        
     </div>
+
+    </div>
+    )
 };
 
 export { LandingComponent };
+
+
