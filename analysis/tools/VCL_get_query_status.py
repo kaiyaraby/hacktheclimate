c1cae498feb2384ded3c44960dd043f9e437ca99
jolpica <47005135@users.noreply.github.com>
2024-03-19 10:38:09 +0100

    update tools

--- analysis/tools/VCL_get_query_status.py
+++ analysis/tools/VCL_get_query_status.py
@@ -1,11 +1,10 @@
 import requests
-import json
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_query_status(query_id,api_key):
+
+def get_query_status(query_id, api_key):
     """
     Get the status of a query.
 
@@ -17,12 +16,10 @@ def get_query_status(query_id,api_key):
         dict: The response JSON containing the query status.
     """
     api_url = f"https://public-test.api.vestas.com/public/vestas-climate-library/v1/querystatus/{query_id}"
-    headers = {
-        "Content-Type": "application/json",
-        "api_key": api_key
-    }
+    headers = {"Content-Type": "application/json", "api_key": api_key}
     response = requests.get(api_url, headers=headers)
     return response
+
 
 # # Example usage
 
--- analysis/tools/VCL_query_timeseries_area.py
+++ analysis/tools/VCL_query_timeseries_area.py
@@ -1,45 +1,47 @@
-import requests
 import json
 
-def timeseries_area_query(min_longitude, max_longitude, min_latitude, max_latitude, start_date, end_date, vars_4d, vars_3d, heights, api_key):
-  """
-  Queries the Vestas Climate Library API for timeseries data.
+import requests
 
-  Args:
-    min_latitude (float): The minimum latitude of the area.
-    max_latitude (float): The maximum latitude of the area.
-    min_longitude (float): The minimum longitude of the area.
-    max_longitude (float): The maximum longitude of the area.
-    start_date (str): The start date of the timeseries data in YYYY-MM-DD format.
-    end_date (str): The end date of the timeseries data in YYYY-MM-DD format.
-    vars_4d (list): List of 4D variables to query.
-    vars_3d (list): List of 3D variables to query.
-    heights (list): List of heights above ground to query.
-    api_key (str): The API key for accessing the Vestas Climate Library API.
 
-  Returns:
-    dict: The response from the API in JSON format.
-  """
-  api_url = "https://public-test.api.vestas.com/public/vestas-climate-library/v1/timeseriesareaquery"
-  headers =  {
-    "Content-Type":"application/json",
-    "api_key": api_key
-  }
-  request = {
-    "minxlat": min_latitude,
-    "maxxlat": max_latitude,
-    "minxlong": min_longitude,
-    "maxxlong": max_longitude,
-    "starttime": start_date,
-    "endtime": end_date,
-    "selectedVariables":{
-        "3DVars":vars_3d,
-        "4DVars":vars_4d, # Can be empty
-        "zHeights":heights # Must not be empty if vars_4d is not empty
+def timeseries_area_query(
+    min_longitude, max_longitude, min_latitude, max_latitude, start_date, end_date, vars_4d, vars_3d, heights, api_key
+):
+    """
+    Queries the Vestas Climate Library API for timeseries data.
+
+    Args:
+      min_latitude (float): The minimum latitude of the area.
+      max_latitude (float): The maximum latitude of the area.
+      min_longitude (float): The minimum longitude of the area.
+      max_longitude (float): The maximum longitude of the area.
+      start_date (str): The start date of the timeseries data in YYYY-MM-DD format.
+      end_date (str): The end date of the timeseries data in YYYY-MM-DD format.
+      vars_4d (list): List of 4D variables to query.
+      vars_3d (list): List of 3D variables to query.
+      heights (list): List of heights above ground to query.
+      api_key (str): The API key for accessing the Vestas Climate Library API.
+
+    Returns:
+      dict: The response from the API in JSON format.
+    """
+    api_url = "https://public-test.api.vestas.com/public/vestas-climate-library/v1/timeseriesareaquery"
+    headers = {"Content-Type": "application/json", "api_key": api_key}
+    request = {
+        "minxlat": min_latitude,
+        "maxxlat": max_latitude,
+        "minxlong": min_longitude,
+        "maxxlong": max_longitude,
+        "starttime": start_date,
+        "endtime": end_date,
+        "selectedVariables": {
+            "3DVars": vars_3d,
+            "4DVars": vars_4d,  # Can be empty
+            "zHeights": heights,  # Must not be empty if vars_4d is not empty
+        },
     }
-  }
-  response = requests.post(api_url, data=json.dumps(request), headers=headers)
-  return response
+    response = requests.post(api_url, data=json.dumps(request), headers=headers)
+    return response
+
 
 # # Example usage:
 # min_longitude = 10
